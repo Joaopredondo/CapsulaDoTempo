8a289e11f3367309394c23824460045d98190547
Joaopredondo <jaop123456789@hotmail.com>
2023-05-16 04:38:02 -0300

    Configuracao ambiente mobile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
-      Oi
+    <div className=" h-screen bg-zinc-900 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
     </div>
   )
 }
