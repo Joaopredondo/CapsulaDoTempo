0363404c1cbc8490149a56ac8586867f623565c0
Joaopredondo <jaop123456789@hotmail.com>
2023-05-19 03:27:41 -0300

    Criacao da autenticacao via GitHub

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import authRoutes from './routes/auth'
 
 const app = fastify()
 
@@ -9,6 +12,7 @@ app.register(cors, {
 })
 
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
