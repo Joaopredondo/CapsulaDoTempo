22a2377ca13386f7f8d6b84cbbae5640bcd361db
Joaopredondo <jaop123456789@hotmail.com>
2023-05-19 03:30:12 -0300

    Divisao das classes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
-import { User } from 'lucide-react'
-import nlwLogo from '../assets/nwl-spacetime-logo.svg'
-import Image from 'next/image'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { SignIn } from '@/components/SignIn'
 
 export default function Home() {
   return (
@@ -12,64 +13,13 @@ export default function Home() {
 
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-        {/* Sign In */}
-        <a
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50 "
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="max-w-[140px] text-sm leading-snug ">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-        {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="Spacetime" />
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="leading-relax text-lg">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-          >
-            CADASTRAR LEMBRANCA
-          </a>
-        </div>
-
-        {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200 ">
-          Feito pelo{' '}
-          <a
-            target="blank"
-            rel="noreferrer"
-            className="underline hover:text-gray-100"
-            href="https://github.com/Joaopredondo"
-          >
-            {' '}
-            João Pedro{' '}
-          </a>
-          com 💜 no NLW da Rocketseat
-        </div>
+        <SignIn />
+        <Hero />
+        <Copyright />
       </div>
       {/* Direita */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50 ">
-              criar agora!
-            </a>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200 ">
+      Feito pelo{' '}
+      <a
+        target="blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://github.com/Joaopredondo"
+      >
+        {' '}
+        João Pedro{' '}
+      </a>
+      com 💜 no NLW da Rocketseat
+    </div>
+  )
+}
